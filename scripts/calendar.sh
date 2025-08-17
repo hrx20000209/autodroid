88e310500d4b3e878eaf93dd474870c07bd1741a
hrx20000209 <70182453+hrx20000209@users.noreply.github.com>
2025-08-17 10:38:30 +0800

    Update calendar.sh

--- scripts/calendar.sh
+++ scripts/calendar.sh
@@ -1,2 +1,1 @@
-droidbot -a apks/com.simplemobiletools.calendar_3.4.2-118_minAPI16.apk -o output/calendar -is_emulator \
--task "create a event of laundry" -keep_env -keep_app
+droidbot -a apks/com.simplemobiletools.calendar_3.4.2-118_minAPI16.apk -o output/calendar -task "create a event of laundry" -keep_env -keep_app
